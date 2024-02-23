8d4f8ad4b2699fbf47be3233e6ebd6bfefa7d142
Tyler Wenger <tylerwenger14@gmail.com>
2024-02-23 18:30:14 -0500

    Randomly target resources

--- role.harvester.js
+++ role.harvester.js
@@ -18,8 +18,9 @@ var roleHarvester = {
                 }
             });
             if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                targetLocationIndex = Math.floor(Math.random() * targets.length);
+                if(creep.transfer(targets[targetLocationIndex], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[targetLocationIndex], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             }
         }
