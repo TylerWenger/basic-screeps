18f8f640c208d97cbc9b34d4cd0dec9f1f3a0066
Tyler Wenger <tylerwenger14@gmail.com>
2024-02-25 08:37:42 -0500

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ module.exports.loop = function () {
     var groups = _.groupBy(Game.creeps, (c) => { return c.memory.role; });
     var sources = Game.spawns['Spawn1'].room.find(FIND_SOURCES);
 
-    console.log(groups);
+    console.log(JSON.stringify(groups));
     if (groups[roleUpgrader]) {
         for (creep in groups[upgrader].length) {
             creep.memory.upgradeAvailable = false;
